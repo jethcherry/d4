807ba4478559d559dcc9390254f9bef086841328
ADMIN <jethrosumbeiywet@gmail.com>
2024-04-10 01:30:30 +0300

    Logic to Generate Password

---
+++ Generate Random Password/script.js
@@ -0,0 +1,31 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"; 
+const numbers = "123456789";   
+const symbols = "@#$%&^*()_~|}[]></-=";
+
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)]; 
+
+    while(length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+createPassword();
+
+
+function copyPassword(){
+
+    passwordBox.select();
+    document.execCommand("copy")
+}
